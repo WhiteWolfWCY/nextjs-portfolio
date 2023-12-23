eeddaa41f838905a91dd60b49fe12c22131791dd
Mateusz Martyna <mateuszmartyna44@gmail.com>
2023-12-23 17:27:17 +0100

    Home component, welcome message, avatar and resume download

--- src/app/components/HomeComponent/HomeComponent.js
+++ src/app/components/HomeComponent/HomeComponent.js
@@ -1,19 +1,71 @@
-import React from 'react'
+import React from "react";
+import { Hind } from "next/font/google";
+
+const hind = Hind({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+});
 
 export default function HomeComponent() {
   return (
     <React.Fragment>
-        <div id="home"
+      <div
+        id="home"
         style={{
-            backgroundImage: "linear-gradient(to right, white 65%, #48AFDE 35%)",
-            minHeight: "500px",
-            maxHeight: "1200px",
-            height: "100%",
-            width: "100%"
+          backgroundImage:
+            "linear-gradient(60deg, #EEF7FB 0 50%, #48AFDE 0% 100%)",
+          minHeight: "500px",
+          maxHeight: "1200px",
+          height: "100%",
+          width: "100%",
         }}
-        >
-
+      >
+        <div className="container m-auto">
+          <div className="grid grid-cols-12">
+            <div className="bg-white flex flex-col justify-center col-span-12 md:col-span-5 md:bg-transparent">
+              <div className="container m-auto">
+                <div className="lg:pl-24 sm:pl-10 pl-0 py-20 md:py-0 md:text-start text-center">
+                  <p
+                    className={`text-[#47AEDE]  text-3xl md:text-base lg:text-2xl ${hind.className}`}
+                  >
+                    Welcome!
+                  </p>
+                  <h1 className="text-[#223740] font-recoletaBlack text-5xl md:text-5xl lg:text-7xl xl:text-7xl mt-5 md:mt-3">
+                    I am Mateusz
+                  </h1>
+                  <h2
+                    className={`text-[#223740] py-2 font-bold uppercase md:text-xl ${hind.className}`}
+                  >
+                    Web Developer &amp; Computer science student
+                  </h2>
+                  <a
+                    href="/#portfolio"
+                    className={`cursor-pointer inline-block bg-[#47AEDE] transition-all duration-300 ease-in-out rounded-lg text-white py-3 px-5 font-bold mt-8 uppercase md:py-2 lg:py-3 lg:px-8 md:text-xs lg:text-base md:mt-5 xl:mt-10 hover:bg-[#223740] hover:show-lg transform hover:translate-y-1 ${hind.className}`}
+                  >
+                    Projects
+                  </a>
+                  <a
+                    href="MateuszMartyna.pdf"
+                    download="MateuszMartyna.pdf"
+                    className={`ml-10 cursor-pointer inline-block bg-[#223740] transition-all duration-300 ease-in-out rounded-lg text-white py-3 px-5 font-bold mt-8 uppercase md:py-2 lg:py-3 lg:px-8 md:text-xs lg:text-base md:mt-5 xl:mt-10 hover:bg-[#47AEDE] hover:show-lg transform hover:translate-y-1 ${hind.className}`}
+                  >
+                    My Resume
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-span-12 pt-[50px] md:bg-transparent bg-[#d9eef7] md:pt-[130px] md:col-span-7">
+              <div className="container m-auto">
+                <img
+                  src="/Avatar.png"
+                  decoding="async"
+                  alt="Profile avatar"
+                ></img>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     </React.Fragment>
-  )
+  );
 }
