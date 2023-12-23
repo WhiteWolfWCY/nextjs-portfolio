ae5c9762d6b971a6b9e27cdcee1e44c46628acfb
Mateusz Martyna <mateuszmartyna44@gmail.com>
2023-12-23 16:28:24 +0100

    Creating home component

---
+++ src/app/components/HomeComponent/HomeComponent.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function HomeComponent() {
+  return (
+    <React.Fragment>
+        <div id="home"
+        style={{
+            backgroundImage: "linear-gradient(to right, white 65%, #48AFDE 35%)",
+            minHeight: "500px",
+            maxHeight: "1200px",
+            height: "100%",
+            width: "100%"
+        }}
+        >
+
+        </div>
+    </React.Fragment>
+  )
+}
